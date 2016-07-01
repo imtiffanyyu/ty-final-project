1941ba5d707dc28c5f5fbc09a4821a103d339779
Tiffany Yu <tiffanyayu@gmail.com>
2016-07-01 19:09:19 +0200

    Updated login page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,49 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
+var Sequelize = require('sequelize');
+var pg = require('pg');
+const bcrypt = require('bcrypt');
+
+// connect to the database
+var sequelize = new Sequelize('fbadmin', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+	host: 'localhost',
+	dialect: 'postgres',
+	define: {
+		timestamps: false
+	}
+});
+
+// create table called admins
+var Admin = sequelize.define('admin', {
+	fbid: Sequelize.BIGINT,
+	name: Sequelize.STRING,
+	email: Sequelize.STRING,
+	picture: Sequelize.STRING
+})
+
+// create table called groups
+var Group = sequelize.define('group', {
+	fbgroupid: Sequelize.BIGINT,
+	name: Sequelize.STRING
+})
+
+// create table called members
+var Member = sequelize.define('member', {
+	fbmemberid: Sequelize.BIGINT,
+	name: Sequelize.STRING,
+	location: Sequelize.STRING,
+	picture: Sequelize.STRING
+})
+
+// declaring relationships
+Admin.hasMany(Group);
+Group.belongsTo(Admin);
+
+Group.hasMany(Member);
+Member.belongsTo(Group);
 
 // Create a new Express application.
 var app = express();
@@ -14,7 +57,12 @@ app.set('view engine', 'pug');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(session({ // middleware is stuff that happens when someone does a request
+	secret: 'oh wow very secret much security',
+	resave: true,
+	saveUninitialized: false
+}));
 
 app.use(express.static('public'));
 
@@ -63,10 +111,193 @@ app.use(passport.session());
 // });
 
 // Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('index');
-  });
+
+// initial log in
+app.get('/', function (req,res) {
+		res.render('index')
+})
+
+app.post('/admin', function (req, res) {
+	fbinfo = req.body.fbinfo
+
+	Admin.findOrCreate( {
+		where: {
+			fbid: fbinfo.id,
+			name: fbinfo.name,
+			email: fbinfo.email,
+			picture: fbinfo.picture.data.url
+		}
+	}).then(function(theadmin) {
+		for (var i = 0; i < fbinfo.admined_groups.data.length; i++) {
+			makeMeGroup(theadmin, fbinfo, i, makeMeMembers)
+		}
+	})
+})
+function makeMeGroup(theadmin, fbinfo, i, callback) {
+	Group.findOrCreate({ where: {
+		fbgroupid: fbinfo.admined_groups.data[i].id,
+		name: fbinfo.admined_groups.data[i].name,
+		adminId: theadmin[0].id
+	}}).then(function(thegroup){
+		callback(thegroup, fbinfo, i)
+	})
+}
+function makeMeMembers(thegroup, fbinfo, i) {
+	console.log('members data')
+	for (var j = 0; j < fbinfo.admined_groups.data[i].members.data.length; j++) {
+		console.log('create a member')
+		doMember(i,j, thegroup, fbinfo)
+	}
+}
+
+function doMember(i,j, thegroup, fbinfo) {
+	console.log('Data j ' + j)
+	console.log(fbinfo.admined_groups.data[i].members.data[j])
+	if (fbinfo.admined_groups.data[i].members.data[j].location != undefined) {
+		Member.findOrCreate({ where: {
+			fbmemberid: fbinfo.admined_groups.data[i].members.data[j].id,
+			name: fbinfo.admined_groups.data[i].members.data[j].name,
+			location: fbinfo.admined_groups.data[i].members.data[j].location.name,
+			picture: fbinfo.admined_groups.data[i].members.data[j].picture.data.url,
+			groupId: thegroup[0].id
+		}})
+	} else {
+		Member.findOrCreate({ where: {
+			fbmemberid: fbinfo.admined_groups.data[i].members.data[j].id,
+			name: fbinfo.admined_groups.data[i].members.data[j].name,
+			picture: fbinfo.admined_groups.data[i].members.data[j].picture.data.url,
+			groupId: thegroup[0].id
+		}})
+	}
+	
+}
+
+app.get('/profile', function (req, res) {
+	Admin.findOne( {
+		where: {id: 1},
+		include: [Group]
+	}).then(function(admininfo) {
+		console.log("bloop")
+		console.log(req.session.admin)
+		var data = admininfo;
+		console.log(data);
+		res.render('profile', {
+			data: data
+		})
+	})
+})
+// app.get('/indexjson',
+// 	function(req, res) {
+// 		res.send(admininfo)
+// });
+
+// // displays group members of a group
+// app.get('/groups/:id', function (req, res) {
+// 	console.log(req.params.id);
+// 	Group.findAll({
+// 		where: {id: req.params.id},
+// 		include: [ Member ]
+// 	}).then(function (group) {
+// 		console.log(group);
+// 		res.render('group')
+// 	});
+// });
+
+
+// creates all the tables to the table
+
+
+
+			// for (var j; j < fbinfo.admined_groups.data[i].members.data.length; j++) {
+			// 	Member.create({
+			// 		fbmemberid: fbinfo.admined_groups.data[i].members.data[j].id,
+			// 		name: fbinfo.admined_groups.data[i].members.data[j].name,
+			// 		location: fbinfo.admined_groups.data[i].members.data[j].location.name,
+			// 		picture: fbinfo.admined_groups.data[i].members.data[j].picture.data.url
+			// 	})
+			// }
+
+// // adds groups to the table
+// app.post('/group', function (req,res) {
+// 	fbinfo = req.body.fbinfo
+// 	Admin.findOne({
+// 		where: {
+
+// 		}
+// 	}).then(function(admin){
+// 		admin.createGroup({
+// 			fbgroupid: fbinfo.
+// 		})
+// 	})
+// })
+
+
+// // create new group
+
+// // create new members
+
+// // update member locations
+
+// // shows profile and groups
+// app.get('/profile', function (req, res) {
+// 	var admin = req.session.admin;
+// 	if (admin === undefined) {
+// 		res.redirect('/?message=' + encodeURIComponent("Please log in to access the blog."));
+// 	} else {
+// 		Group.findAll({
+// 			where: {
+// 				adminId: admin.id
+// 			}
+// 		}).then(function(groups) {
+// 			var groups = groups;
+// 			console.log(data)
+// 		})
+// 		res.render('profile', {
+// 			admin: admin,
+// 			groups: groups
+// 		})
+// 	}
+// })
+
+// app.post('/login', function (req, res) {
+// 	if(req.body.username.length === 0) {
+// 		res.redirect('/?message=' + encodeURIComponent("Please fill out your username."));
+// 		return;
+// 	}
+
+// 	if(req.body.password.length === 0) {
+// 		res.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
+// 		return;
+// 	}
+
+// 	Admin.findOne({
+// 		where: {
+// 			username: req.body.username
+// 		}
+// 	}).then(function (admin) {
+// 		if (admin !== null) {
+// 			var hash = admin.password;
+// 			bcrypt.compare (req.body.password, hash, function (err, result) {
+// 				req.session.admin = admin;
+// 				res.redirect('/profile')
+// 			})
+// 		} else {
+// 			res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+// 		}
+// 	}, function (err) {
+// 			res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+// 	})
+// })
+
+// app.get('/logout', function (req, res) {
+// 	req.session.destroy(function(err) {
+// 		if(err) {
+// 			throw err;
+// 		}
+// 		res.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+// 	})
+// });
+
 
 // app.get('/login',
 //   function(req, res){
@@ -93,6 +324,8 @@ app.get('/',
 //     res.render('profile', { user: req.user });
 //   });
 
-var server = app.listen(3000, function () {
-	console.log('Diversability app listening on port: ' + server.address().port);
+sequelize.sync( {force: true} ).then(function () {
+	var server = app.listen(3000, function () {
+		console.log('Diversability app listening on port: ' + server.address().port);
+	});
 });
